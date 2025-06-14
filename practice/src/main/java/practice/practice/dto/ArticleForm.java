22e74a55b97cfc43b2817a28a7aa456797102677
김경민 <hdh985@naver.com>
2025-06-14 06:36:24 -0700

    feat: 게시판 기능 완성

--- practice/src/main/java/practice/practice/dto/ArticleForm.java
+++ practice/src/main/java/practice/practice/dto/ArticleForm.java
@@ -1,36 +1,27 @@
 package practice.practice.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 import practice.practice.entity.Article;
 import practice.practice.repository.ArticleRepository;
 
+
+
+@AllArgsConstructor
+@ToString
 public class ArticleForm {
 
     private ArticleRepository articleRepository;
+    private Long id;
 
     private String title;
     private String content;
 
-    public ArticleForm(String title, String content) {
-
-        this.title = title;
-        this.content = content;
-
-    }
-
-    @Override
-
-    public String toString() {
-
-        return "ArticleForm {" +
-                "title='" + title + '\'' +
-                ",content='" + content + '\'' +
-                '}';
-
-
-    }
 
     public Article toEntity() {
 
-        return new Article(null, title, content);
+        return new Article(id, title, content);
     }
 }
--- practice/src/main/java/practice/practice/repository/ArticleRepository.java
+++ practice/src/main/java/practice/practice/repository/ArticleRepository.java
@@ -3,6 +3,10 @@ package practice.practice.repository;
 import org.springframework.data.repository.CrudRepository;
 import practice.practice.entity.Article;
 
+import java.util.ArrayList;
+
 public interface ArticleRepository extends CrudRepository<Article,Long> {
 
+    @Override
+    ArrayList<Article> findAll();
 }
